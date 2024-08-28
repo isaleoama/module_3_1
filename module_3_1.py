4b72bee8a90c5e18c012fb2978c4cc7543906d5d
isa <alfred.nikitin@Mail.ru>
2024-08-28 08:48:24 +0300

    Add files via upload

---
+++ module_3_1.py
@@ -0,0 +1,28 @@
+calls = 0
+
+
+def count_calls():
+    global calls
+    calls += 1
+
+
+def string_info(string):
+    global calls
+    lens = len(string)
+    stringUp = string.upper()
+    stringLow = string.lower()
+    print(f"{lens},{stringUp},{stringLow}")
+    count_calls()
+
+
+def is_contains(string, list_to_search):
+    global calls
+    count_calls()
+    return string.upper() in [s.upper() for s in list_to_search]
+
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))  # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic']))  # No matches
+print(calls)
